4d4b8e66b1808b8103f2e12cf1f385fda9b82b91
matvs <6883643+matvs@users.noreply.github.com>
2019-08-07 18:45:06 +0200

    Add basic physics to spaceship motion

--- js/asteroids.js
+++ js/asteroids.js
@@ -9,46 +9,46 @@ var AsteroidsGame = {
     },
     spaceShip: null,
 
-    init: function(){
+    init: function () {
         this.onKeyDownHandler = this.onKeyDownHandler.bind(this);
         this.onKeyUpHandler = this.onKeyUpHandler.bind(this);
 
         return this;
     },
 
-    start: function(optionsArg = {}){
+    start: function (optionsArg = {}) {
         this.options = Object.assign(this.defaultOptions, optionsArg);
-        
+
         this.removeKeyEvents();
         cancelAnimationFrame(this.requestId);
 
         this.canvas = document.getElementById(this.options.canvasId);
-        if(!this.canvas || this.canvas.nodeName != "CANVAS"){
+        if (!this.canvas || this.canvas.nodeName != "CANVAS") {
             throw "No canvas found or found element is not canvas"
         }
-        
+
         this.draw = this.draw.bind(this);
 
         this.ctx = this.canvas.getContext('2d');
-        this.spaceShip = new SpaceShip(this.ctx);
+        this.spaceShip = new SpaceShip(this.ctx,this.canvas.width/2, this.canvas.height/2);
 
         this.requestId = requestAnimationFrame(this.draw)
 
         this.bindKeyEvents()
     },
 
-    draw: function(){
-        this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
+    draw: function () {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.spaceShip.draw()
         this.requestId = requestAnimationFrame(this.draw)
     },
 
-    onKeyDownHandler: function(e){
+    onKeyDownHandler: function (e) {
         e.preventDefault();
-        this.spaceShip.move(e.keyCode);
+        this.spaceShip.start(e.keyCode, e.repeat);
     },
-    
-    onKeyUpHandler: function(e){
+
+    onKeyUpHandler: function (e) {
         e.preventDefault();
         this.spaceShip.stop(e.keyCode);
     },
@@ -65,7 +65,7 @@ var AsteroidsGame = {
 }.init();
 
 
-function SpaceShip(ctx){
+function SpaceShip(ctx, x, y) {
     var self = this;
 
     self.ctx = ctx;
@@ -73,21 +73,24 @@ function SpaceShip(ctx){
     self.state = {
 
     }
-    
-    self.x = 0;
-    self.y = 0;
-    self.angle = 0;
 
+    self.x = x || 0;
+    self.y = y || 0;
+    self.angle = 0;
+    self.vAngle = (Math.PI / 2);
+
+    var step = 1;
+    var stepX = stepY = 0;
     var a = 10;
     var h = 20;
-    self.draw = function(){
+    self.draw = function () {
         self.updateCoords();
         let ctx = self.ctx;
         ctx.save();
-        let r = (1.0/8.0)*(a*a/h) + 0.5*h;
-        ctx.translate(self.x + 0.5 * a, self.y - (h-r));
+        let r = (1.0 / 8.0) * (a * a / h) + 0.5 * h;
+        ctx.translate(self.x + 0.5 * a, self.y - (h - r));
         ctx.rotate(self.angle);
-        ctx.translate(-(self.x + 0.5 * a), -(self.y - (h-r)));
+        ctx.translate(-(self.x + 0.5 * a), -(self.y - (h - r)));
         ctx.beginPath();
         ctx.moveTo(self.x, self.y);
         ctx.lineTo(self.x + a, self.y);
@@ -95,111 +98,72 @@ function SpaceShip(ctx){
         ctx.lineTo(self.x, self.y);
         ctx.stroke();
         ctx.restore()
-    },
+    }
 
-    self.move = function(direction){
-       switch(direction){
-           case KEY_MAP.UP:{
-            self.state.goingUp = true;
-            break;
-           }
-           case KEY_MAP.DOWN:{
-            self.state.goingDown = true;
-            break;
-           }
-           case KEY_MAP.LEFT:{
-            self.state.goingLeft = true;
-            break;
-           }
-           case KEY_MAP.RIGHT:{
-            self.state.goingRight = true;
-            break;
-           }
-           case KEY_MAP.c:{
-            self.state.rotatingClockWise = true;
-            break;
-           }
-           case KEY_MAP.x:{
-            self.state.rotatingCounterClockWise = true;
-            break;
-           }
-       }
+    self.start = function (direction, repeat) {
+        switch (direction) {
+            case KEY_MAP.UP: {
+                self.state.moving = true;
+                if (!repeat) {
+                    self.vAngle = self.angle + (Math.PI / 2);
+                    stepY = Math.sin(self.vAngle) * step;
+                    stepX = Math.cos(self.vAngle) * step
+                }
+                break;
+            }
+            case KEY_MAP.LEFT: {
+                self.state.rotatingCounterClockWise = true;
+                break;
+            }
+            case KEY_MAP.RIGHT: {
+                self.state.rotatingClockWise = true;
+                break;
+            }
+        }
     }
 
 
-    self.stop = function(direction){
-        switch(direction){
-            case KEY_MAP.UP:{
-             self.state.goingUp = false;
-             break;
+    self.stop = function (direction) {
+        switch (direction) {
+            case KEY_MAP.UP: {
+                //self.state.moving = false;
+                break;
             }
-            case KEY_MAP.DOWN:{
-             self.state.goingDown = false;
-             break;
+            case KEY_MAP.LEFT: {
+                self.state.rotatingCounterClockWise = false;
+                break;
             }
-            case KEY_MAP.LEFT:{
-             self.state.goingLeft = false;
-             break;
-            }
-            case KEY_MAP.RIGHT:{
-             self.state.goingRight = false;
-             break;
-            }
-            case KEY_MAP.c:{
-             self.state.rotatingClockWise = false;
-             break;
-            }
-            case KEY_MAP.x:{
-             self.state.rotatingCounterClockWise = false;
-             break;
+            case KEY_MAP.RIGHT: {
+                self.state.rotatingClockWise = false;
+                break;
             }
         }
-     }
+    }
 
-    self.updateCoords=function(){
-        if(self.state.goingUp){
-            moveUp();
-        }
-        if(self.state.goingDown){
-            moveDown()
-        }
-        if(self.state.goingLeft){
-            moveLeft()
-        }
-        if(self.state.goingRight){
-            moveRight()
-        }
-        if(self.state.rotatingClockWise){
+    self.updateCoords = function () {
+        if (self.state.rotatingClockWise) {
             rotateClockWise()
         }
-        if(self.state.rotatingCounterClockWise){
+        if (self.state.rotatingCounterClockWise) {
             rotateCounterClockWise();
+        }
+        if (self.state.moving) {
+            move();
         }
     }
 
-    rotateClockWise = function(){
+    rotateClockWise = function () {
         self.angle += (Math.PI / 180) * 5;
     }
 
-    rotateCounterClockWise = function(){
+    rotateCounterClockWise = function () {
         self.angle -= (Math.PI / 180) * 5;
     }
 
-    var step = 10;
-    moveUp = function(){
-        self.y -= step;
-    }
 
-    moveDown = function(){
-        self.y += step;
-    }
-
-    moveLeft = function(){
-        self.x -= step;
-    }
-
-    moveRight = function(){
-        self.x += step;
+    move = function () {
+        self.y -= stepY;
+        self.x -= stepX
     }
 }
 
@@ -209,6 +173,6 @@ const KEY_MAP = {
     RIGHT: 39,
     DOWN: 40,
     SPACE: 32,
-    x:88,
-    c:67
+    x: 88,
+    c: 67
 } 
